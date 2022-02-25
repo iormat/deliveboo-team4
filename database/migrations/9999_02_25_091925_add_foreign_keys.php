2beac55b3f31624f73daf462bfff9cba5a91584d
Orazio <orazioventrella@hotmail.it>
2022-02-25 10:36:13 +0100

    feat: create relation table

---
+++ database/migrations/9999_02_25_091925_add_foreign_keys.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('type_user', function (Blueprint $table) {
+            
+            $table -> foreign('type_id', 'type_id_user') -> references('id') -> on('types');
+            $table -> foreign('user_id', 'user_id_type') -> references('id') -> on('users');
+
+        });
+        // Schema::table('type_user', function (Blueprint $table) {
+            
+        //     $table -> foreign('user_id', 'user_id_type') -> references('id') -> on('users');
+
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('type_user', function(Blueprint $table) {
+            $table -> dropForeign('type_id_user');
+            $table -> dropForeign('user_id_type');
+        });
+    }
+}
