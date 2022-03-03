9e8743df0e621b653921d0422440c37c38555933
Dani9898 <danieleoliva98@gmail.com>
2022-03-03 17:51:03 +0100

    added Create dish Component

--- resources/js/components/CreateComponent.vue
+++ resources/js/components/CreateComponent.vue
@@ -1,95 +1,88 @@
 <template>
 
         <!-- <section> -->
-        <section v-if="createForm" id="create">
-            <h1>funzionaa</h1>
+        <section id="create">
             <form method="POST" enctype="multipart/form-data" @submit.prevent="submitDish">
+                <!-- dish name - create -->
                 <label for="name">
-                    Inserisci il nome
+                    Inserisci il nome&colon;
                     <br>
                     <input type="text" id="name" v-model="dish_name" min="4" max="50" required>
                 </label>
                 <br>
+                <!-- dish description - create -->
                 <label for="desription">
-                    Inserisci il una descrizione
+                    Inserisci il una descrizione&colon;
                     <br>
                     <textarea v-model="description" id="desription" cols="50" rows="5" required></textarea>
                 </label>
                 <br>
+                <!-- dish price - create -->
                 <label for="price">
-                    Inserisci il prezzo
+                    Inserisci il prezzo&colon;
                     <br>
                     <input type="number" min="0.00" max="999.99" step="0.01" id="price" v-model="price" required>
                 </label>
                 <br>
+                <!-- dish ingredients - create -->
                 <label for="ingredients">
-                    Aggiungi gli ingredienti
+                    Aggiungi gli ingredienti&colon;
                     <br>
                     <textarea v-model="ingredients" id="ingredients" cols="50" rows="5" required></textarea>
                 </label>
                 <br>
-                
-
-                 <label for="img">
-                    Aggiungi immagine
+                <!-- dish img - create -->
+                <label for="img">
+                    Aggiungi immagine&colon;
                     <br>
                     <input type="file" @change="saveImg" id="img">
                 </label> 
                 <br>
-
+                <!-- categories select - create -->
                 <select v-model="category" class="my-3" name="category">
                     <option v-for="category in categories" :key="category.id" :value="category.id" >
                         {{category.category_name}}
                         {{category.id}}
                     </option>
-                </select>
-                
+                </select> 
                 <br>
 
-                <input type="submit" class="mb-5 btn btn-success" value="submit"> 
-
+                <input type="submit" class="mb-5 btn btn-success" value="submit">
             </form>
-
         </section>
 
 </template>
 <script>
 export default {
     data: function(){
-        return{
-            dishes: [],
-
+        return{     
+            available: true,
             dish_name: '',
             description: '',
             price: 0,
             ingredients: '',
             category: -1,
             dishes_img: "",
-            data: {},        
-            
+
+            newDishes: [],
         };
     },
 
     props: {
-        createForm: Boolean,
-        categories: Array
+        dishes: Array,
+        categories: Array,
     },
 
     methods: {
-
-        // newDish() {
-        //     this.createForm = !this.createForm;
-        // },
-
         saveImg(img) {
             this.dishes_img = img.target.files[0];
             console.log("dishes_img:", this.dishes_img);
-
         },
-
+       
+       // submit create dish - form
         submitDish(e) {
             let form = new FormData(e.target);
-                form.append("dish_name",this.dish_name);
+                form.append("dish_name", this.dish_name);
                 form.append("description", this.description);
                 form.append("price", this.price);
                 form.append("ingredients", this.ingredients);
@@ -98,27 +91,27 @@ export default {
                 };
                 form.append("category", this.category);
                 form.append("user_id", this.user_id);
+            // post form 
 
             let self = this;
-
             axios.post('/api/store', form)
             .then(function (response) {
-                self.dishes.push(response.data)
-                console.log("api create:", response.data);
+                self.newDishes = self.dishes;
+                self.newDishes.push(response.data);
+                // console.log("api create:", self.newDishes);
+                self.$emit("getNewDishes", self.newDishes);
             })
             .catch(function (error) {
-                console.log(error);
+                console.error(error);
             });
 
-            this.dish_name =   this.description = this.ingredients = this.dishes_img ='';
+            // reset all dish properties
+            this.dish_name = this.description = this.ingredients = this.dishes_img = '';
             this.price = this.category = 0;
-            this.data = {};
             this.createForm = !this.createForm;
 
-            this.$emit('update:createForm', e.createForm);
-            this.$emit('ArrayPush', dishes);
-
-        }
+            this.$emit("createNewDish");
+        },
     },
 }
 </script>
--- resources/js/components/DashboardMenuComponent.vue
+++ resources/js/components/DashboardMenuComponent.vue
@@ -4,63 +4,23 @@
         <!-- button to toggle create - form -->
         <div @click="createNewDish" class="mb-2 btn btn-success">Create</div>        
         <button @click="check" >CHECK</button>
-        <!-- <CreateComponent/> -->
         <!-- <create-component                               
             v-bind:createForm.sync="createForm" 
             :categories="categories"
             @update:arrayPush="arrayPush">
         </create-component> -->
+        <create-component 
+        :dishes="dishes"
+        :categories="categories"
+        v-if="createForm"
+        @createNewDish="createNewDish"
+        @getNewDishes="getNewDishes"
+
+        >
+        </create-component>
 
         <!-- create new dish - form -->
-        <section v-if="createForm" id="create">
-            <form method="POST" enctype="multipart/form-data" @submit.prevent="submitDish">
-                <!-- dish name - create -->
-                <label for="name">
-                    Inserisci il nome&colon;
-                    <br>
-                    <input type="text" id="name" v-model="dish_name" min="4" max="50" required>
-                </label>
-                <br>
-                <!-- dish description - create -->
-                <label for="desription">
-                    Inserisci il una descrizione&colon;
-                    <br>
-                    <textarea v-model="description" id="desription" cols="50" rows="5" required></textarea>
-                </label>
-                <br>
-                <!-- dish price - create -->
-                <label for="price">
-                    Inserisci il prezzo&colon;
-                    <br>
-                    <input type="number" min="0.00" max="999.99" step="0.01" id="price" v-model="price" required>
-                </label>
-                <br>
-                <!-- dish ingredients - create -->
-                <label for="ingredients">
-                    Aggiungi gli ingredienti&colon;
-                    <br>
-                    <textarea v-model="ingredients" id="ingredients" cols="50" rows="5" required></textarea>
-                </label>
-                <br>
-                <!-- dish img - create -->
-                <label for="img">
-                    Aggiungi immagine&colon;
-                    <br>
-                    <input type="file" @change="saveImg" id="img">
-                </label> 
-                <br>
-                <!-- categories select - create -->
-                <select v-model="category" class="my-3" name="category">
-                    <option v-for="category in categories" :key="category.id" :value="category.id" >
-                        {{category.category_name}}
-                        {{category.id}}
-                    </option>
-                </select> 
-                <br>
-
-                <input type="submit" class="mb-5 btn btn-success" value="submit">
-            </form>
-        </section>
+        
         <!--  table to display user menu -->
         <table border="2">
             <tr>
@@ -214,6 +174,16 @@ export default {
         createNewDish() {
             this.createForm = !this.createForm;
         },
+
+        // get new dish from CreateComponent
+        getNewDishes(newDishes) {
+            // console.log("dishes prima", this.dishes);
+            console.log("asd",newDishes);
+            this.dishes = newDishes;
+            // console.log("dishes dopo", this.dishes);
+
+        },
+
         // toggle close edit - form
         toggleEditDish() {
             this.changeForm = !this.changeForm;
@@ -245,10 +215,7 @@ export default {
             this.changeForm = !this.changeForm;
         },
         // save user img - useful format
-        saveImg(img) {
-            this.dishes_img = img.target.files[0];
-            console.log("dishes_img:", this.dishes_img);
-        },
+        
         // save updated user img - useful format
         saveUpdatedImg(img) {
             this.editDishes_img = img.target.files[0];
@@ -256,36 +223,8 @@ export default {
         },
         // test button
         check() {
-            let x = document.getElementById("prova").checked;
-            console.log("check", x);
+            console.log("check", this.dishes);
 
-        },
-        // submit create dish - form
-        submitDish(e) {
-            let form = new FormData(e.target);
-                form.append("dish_name", this.dish_name);
-                form.append("description", this.description);
-                form.append("price", this.price);
-                form.append("ingredients", this.ingredients);
-                if(this.dishes_img != ''){
-                  form.append("dishes_img",this.dishes_img);  
-                };
-                form.append("category", this.category);
-                form.append("user_id", this.user_id);
-            // post form 
-            let self = this;
-            axios.post('/api/store', form)
-            .then(function (response) {
-                self.dishes.push(response.data)
-                console.log("api create:", response.data);
-            })
-            .catch(function (error) {
-                console.error(error);
-            });
-            // reset all dish properties
-            this.dish_name = this.description = this.ingredients = this.dishes_img = '';
-            this.price = this.category = 0;
-            this.createForm = !this.createForm;
         },
     
         // update existing dish
