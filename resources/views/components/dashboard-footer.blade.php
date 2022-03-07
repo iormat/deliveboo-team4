8f06eca2677c4fd20a47446fd0f8ee71decaa894
Orazio <orazioventrella@hotmail.it>
2022-03-07 19:13:24 +0100

    feat: create footer

---
+++ resources/views/components/dashboard-footer.blade.php
@@ -0,0 +1,39 @@
+<footer>
+    <div class="container ">
+            
+        <div class="footer-top row justify-content-between ">
+                <div class="dowload ">
+                    <span>Deliveboo</span>
+                    <div>
+                        <a href="*"><img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/6a17d190539d6845b10955e8ba585d36.svg" alt=""></a>
+                        <a href="*"><img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/75a8c9ee29967730c0674e7fa870e582.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="navbar-footer ">
+                    <h5>Note Legali</h5>
+                    <ul>
+                        <li><a href="*">Privacy</a></li>
+                        <li><a href="*">Cookie</a></li>
+                        <li><a href="*">Termini & condizioni</a></li>
+                        <li><a href="*">Politiche</a></li>
+                    </ul>
+                </div>
+                <div class="subscribe">
+                    <h5>Vuoi il tuo ristorante su Deliveboo?</h5>
+                    <a href="*">Registrati</a>
+                </div>
+        </div>
+    
+        
+            <div class="row row-cols align-items-center justify-content-between footer-bottom ">
+                <div class="social">
+                    <a href="*"><i id="facebook" class="fab fa-facebook-square"></i></a>
+                    <a href="*"><i id="twitter" class="fab fa-twitter-square"></i></a>
+                    <a href="*"><i id="instagram" class="fab fa-instagram"></i></a>
+                </div>
+                <div class="copyright">
+                    <i class="far fa-copyright"></i><span>2022 Deliveroo</span>
+                </div>
+            </div>     
+    </div>
+</footer>
--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -6,17 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>DeliveBoo</title>
 </head>
 <body>
     <div id="app">
         <div>
-            @include('components.header')
+            
             @yield('content')
         </div>
-
+       
     </div>
     
-
+    @include('components.dashboard-footer')
 </body>
 </html>
