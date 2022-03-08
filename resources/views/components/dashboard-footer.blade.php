6383e2ad286ca23637c0fc3662553e34243bab72
Orazio <orazioventrella@hotmail.it>
2022-03-08 13:07:55 +0100

    fix: footer responsive

--- resources/views/components/dashboard-footer.blade.php
+++ resources/views/components/dashboard-footer.blade.php
@@ -2,13 +2,16 @@
     <div class="container ">
             
         <div class="footer-top row justify-content-between ">
+            <div class="col-sm-12 col-lg-6 mt-3 p-1">
                 <div class="dowload ">
-                    <span>Deliveboo</span>
+                    <h5>Deliveboo</h5>
                     <div>
                         <a href="*"><img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/6a17d190539d6845b10955e8ba585d36.svg" alt=""></a>
                         <a href="*"><img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/75a8c9ee29967730c0674e7fa870e582.png" alt=""></a>
                     </div>
                 </div>
+            </div>
+            <div class="col-sm-12 col-lg-3 mt-3 p-1">
                 <div class="navbar-footer ">
                     <h5>Note Legali</h5>
                     <ul>
@@ -18,22 +21,25 @@
                         <li><a href="*">Politiche</a></li>
                     </ul>
                 </div>
+            </div>
+            <div class="col-sm-12 col-lg-3 mt-3 p-1">
                 <div class="subscribe">
                     <h5>Vuoi il tuo ristorante su Deliveboo?</h5>
                     <a href="*">Registrati</a>
                 </div>
+            </div>
+            
         </div>
-    
-        
-            <div class="row row-cols align-items-center justify-content-between footer-bottom ">
-                <div class="social">
-                    <a href="*"><i id="facebook" class="fab fa-facebook-square"></i></a>
-                    <a href="*"><i id="twitter" class="fab fa-twitter-square"></i></a>
-                    <a href="*"><i id="instagram" class="fab fa-instagram"></i></a>
-                </div>
-                <div class="copyright">
-                    <i class="far fa-copyright"></i><span>2022 Deliveroo</span>
-                </div>
-            </div>     
+
+        <div class="row align-items-center justify-content-between footer-bottom p-1">
+            <div class="social g-5">
+                <a href="*"><i id="facebook" class="fab fa-facebook-square"></i></a>
+                <a href="*"><i id="twitter" class="fab fa-twitter-square"></i></a>
+                <a href="*"><i id="instagram" class="fab fa-instagram"></i></a>
+            </div>
+            <div class="copyright">
+                <i class="far fa-copyright"></i><span>2022 Deliveboo</span>
+            </div>
+        </div>     
     </div>
 </footer>
