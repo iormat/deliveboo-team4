38f6502e744ac4def277f11a1ae71555b8eb63be
iormat <iorio436@outlook.it>
2022-03-09 10:21:50 +0100

    minor: Homepage Component refactoring

--- resources/js/components/homepageComponent.vue
+++ resources/js/components/homepageComponent.vue
@@ -1,9 +1,11 @@
 <template>
     <section id="restaurants">
+        <!-- search for restaurant  type -->
         <select name="" id="" v-model="selectType">
             <option value="Tutti" selected>Tutti</option>
             <option v-for="type in types" :key="type.id" :value="type.id">{{type.type_name}}</option>
         </select>
+        <!-- get restaurant to display -->
         <div class="container">
             <ul class="row">
                 <li v-for="restaurant in showFiltered" :key="restaurant.id" class="col-sm-12 col-md-6 col-lg-4">
@@ -32,49 +34,46 @@
 <script>
 export default {
 
-data: function() {
-    return {
-        types : [],
-        selectType : 'Tutti',
-        restaurantsArr : []
-    }
-},
-props: {
-        restaurants: Array,
+    data: function() {
+        return {
+            types : [],
+            selectType : 'Tutti',
+        }
     },
+
+    props: {
+            restaurants: Array,
+        },
     
-methods: {
-    goToRestaurant(id) {
-        axios.get(`/restaurant/details/${id}`)
-        .then(res => {
-            window.location.href = `/restaurant/details/${id}`;
-        })
-        .catch(err => {
-            console.log(err);
-        })
+    methods: {
+        // redirect to restaurant details page
+        goToRestaurant(id) {
+            axios.get(`/restaurant/details/${id}`)
+            .then(res => {
+                window.location.href = `/restaurant/details/${id}`;
+            })
+            .catch(err => {
+                console.log(err);
+            })
         }
     },
     mounted() {
         axios.get('/api/restaurants/types')
         .then(res => {
-            this.types = res.data
+            this.types = res.data;
         })
-    },
-    created() {
-        this.restaurantsArr = this.restaurants
-        console.log(this.restaurantsArr[0].types[0].type_name);
     },
     computed: {
         // check array to get only related results
         showFiltered() {
             if(this.selectType === 'Tutti') {
-                return this.restaurantsArr;
+                return this.restaurants;
             }
-            return this.restaurantsArr.filter((item) => {
-                for (let i = 0; i < item.types.length; i++) {
-                    let type = item.types[i]
+            return this.restaurants.filter((restaurant) => {
+                for (let i = 0; i < restaurant.types.length; i++) {
+                    let type = restaurant.types[i];
                     if(type.id === this.selectType){
-                        return this.restaurantsArr;
+                        return this.restaurants;
                     }
                 }   
             })
@@ -82,7 +81,3 @@ methods: {
     }
 }
 </script>
-
-<style>
-
-</style>
