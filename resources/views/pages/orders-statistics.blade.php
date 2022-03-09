c8a02304b5ec60b8968a2376f25d2d921614efdc
Dani9898 <danieleoliva98@gmail.com>
2022-03-09 19:05:39 +0100

    feat: year charts finiti

--- resources/views/pages/orders-statistics.blade.php
+++ resources/views/pages/orders-statistics.blade.php
@@ -2,16 +2,20 @@
 
 @section('content')
 
-<div class="container">    
+<div class="charts">    
+    <h1>Statistiche Mensili</h1>
     <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-4 g-5"><chart-month-component></chart-month-component></div>
-        <div class="col-sm-12 col-md-6 col-lg-4 g-5"><chart-month-orders-component></chart-month-orders-component></div>
-        <div class="col-sm-12 col-md-6 col-lg-4 g-5"><chart-month-profit-component></chart-month-profit-component></div>
-        <div class="col-sm-12 col-md-6 col-lg-4 g-5"><chart-year-component></chart-year-component></div>
-        <div class="col-sm-12 col-md-6 col-lg-4 g-5"><chart-year-orders-component></chart-year-orders-component></div>
-        <div class="col-sm-12 col-md-6 col-lg-4 g-5"><chart-year-profit-component></chart-year-profit-component></div>
+        <div class="col-sm-12 col-md-6 col-lg-4 g-5 my-5"><chart-month-component></chart-month-component></div>
+        <div class="col-sm-12 col-md-6 col-lg-4 g-5 my-5"><chart-month-orders-component></chart-month-orders-component></div>
+        <div class="col-sm-12 col-md-6 col-lg-4 g-5 my-5"><chart-month-profit-component></chart-month-profit-component></div>
     </div>
-
+    <h1>Statistiche Annuali</h1>
+    
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-4 g-5 my-5"><chart-year-component></chart-year-component></div>
+        <div class="col-sm-12 col-md-6 col-lg-4 g-5 my-5"><chart-year-orders-component></chart-year-orders-component></div>
+        <div class="col-sm-12 col-md-6 col-lg-4 g-5 my-5"><chart-year-profit-component></chart-year-profit-component></div>
+    </div>
 </div>
  
 
