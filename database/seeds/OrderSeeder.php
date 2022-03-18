46ba98e41de8e31305493e4527e26806373c843a
iormat <iorio436@outlook.it>
2022-03-18 15:57:10 +0100

    fix: total price based on dishes in order

--- database/seeds/OrderSeeder.php
+++ database/seeds/OrderSeeder.php
@@ -14,22 +14,37 @@ class OrderSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        factory(Order::class, 10) -> make() -> each(function($order) {
+        factory(Order::class, 100) -> make() -> each(function($order) {
             // link customers to orders
             $customer = Customer::inRandomOrder() -> limit(1) -> first();
             $order -> customer() -> associate($customer);
-
             $order -> save();
 
             // link dishes to order
-            $dishes = Dish::where('user_id', rand(1, 5)) -> limit(rand(1, 20)) -> get();
+            $dishes = Dish::where('user_id', rand(1, 5)) -> limit(rand(1, 50)) -> get();
             $order -> dishes() -> attach($dishes);
-            $order -> save(); 
+            $order -> update(); 
 
-            // // get amount value
-            DB::table('dish_order') -> update([
-                'amount' => rand(1, 4)
-            ]);            
+            // get amount value
+            foreach ($dishes as $dish) {
+                DB::table('dish_order') 
+                    -> where('dish_order.dish_id', $dish -> id)
+                    -> update([
+                        'amount' => rand(1, 5)
+                    ]);
+            };
+
+            // get real total price based on dishes
+            $totalPrice = DB::table('orders')
+                -> join('dish_order', 'orders.id', 'dish_order.order_id')
+                -> join('dishes', 'dish_order.dish_id', 'dishes.id')
+                -> groupBy('orders.id')
+                -> where('dish_order.order_id', $order -> id)
+                -> sum('dishes.price', '*', 'dish_order.amount');
+            // save total
+            $order['total_price'] = $totalPrice; 
+
+            $order -> update();
         });
     }
 }
